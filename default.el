5c3d30994e19881ebc45ecdab7b7991023bd2560
Wesley Nelson <wgn@wesnel.dev>
2023-08-03 18:55:39 -0700

    Update config

--- default.el
+++ default.el
@@ -40,16 +40,16 @@
     :ensure t
 
     :config
-    (dolist (var '("SSH_AUTH_SOCK"
-                   "SSH_AGENT_PID"
-                   "GPG_AGENT_INFO"
-                   "GPG_TTY"
-                   "LANG"
-                   "LC_CTYPE"
-                   "NIX_SSL_CERT_FILE"
-                   "NIX_PATH"))
-      (add-to-list 'exec-path-from-shell-variables var))
     (when (memq window-system '(mac ns x))
+      (dolist (var '("SSH_AUTH_SOCK"
+                     "SSH_AGENT_PID"
+                     "GPG_AGENT_INFO"
+                     "GPG_TTY"
+                     "LANG"
+                     "LC_CTYPE"
+                     "NIX_SSL_CERT_FILE"
+                     "NIX_PATH"))
+        (add-to-list 'exec-path-from-shell-variables var))
       (exec-path-from-shell-initialize)))
 
   ;; Avoid putting files in weird places.
