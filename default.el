ddf1b84d3a95304d87b778243bbbac8d6d60b886
Wesley Nelson <wgn@wesnel.dev>
2023-08-03 18:51:49 -0700

    Update config

--- default.el
+++ default.el
@@ -43,6 +43,7 @@
     (dolist (var '("SSH_AUTH_SOCK"
                    "SSH_AGENT_PID"
                    "GPG_AGENT_INFO"
+                   "GPG_TTY"
                    "LANG"
                    "LC_CTYPE"
                    "NIX_SSL_CERT_FILE"
