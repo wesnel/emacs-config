67daacf2bac6755da84aa3425c7d5b6a5af083eb
Wesley Nelson <wgn@wesnel.dev>
2024-06-22 17:12:46 -0700

    Add extra golang/delve configuration for dape

--- default.el
+++ default.el
@@ -665,6 +665,9 @@
 (use-package dape
   :ensure t
 
+  :defines
+  (dape-configs)
+
   :commands
   (dape
    dape-breakpoint-global-mode
@@ -843,7 +846,39 @@
   (add-hook 'go-ts-mode-hook #'wgn/go-ts-mode-eglot-setup)
 
   ;; CamelCase aware editing operations.
-  (add-hook 'go-ts-mode-hook #'subword-mode))
+  (add-hook 'go-ts-mode-hook #'subword-mode)
+
+  :config
+  ;; Configure dape for delve:
+  (add-to-list
+   'dape-configs
+   `(delve-unit-test
+     modes (go-mode go-ts-mode)
+     ensure dape-ensure-command
+     fn dape-config-autoport
+     command "@dlv@"
+     command-args ("dap" "--listen" "127.0.0.1::autoport")
+     command-cwd dape-cwd-fn
+     port :autoport
+     :type "debug"
+     :request "launch"
+     :mode (lambda ()
+             (if (string-suffix-p "_test.go" (buffer-name))
+                 "test"
+               "debug"))
+     :cwd dape-cwd-fn
+     :program (lambda ()
+                (if (string-suffix-p "_test.go" (buffer-name))
+                    (concat "./" (file-relative-name default-directory (funcall dape-cwd-fn)))
+                  (funcall dape-cwd-fn)))
+     :args (lambda ()
+             (require 'which-func)
+             (if (string-suffix-p "_test.go" (buffer-name))
+                 (when-let* ((test-name (which-function))
+                             (test-regexp (concat "^" test-name "$")))
+                   (if test-name `["-test.run" ,test-regexp]
+                     (error "No test selected"))))
+             []))))
 
 ;;;; Extra Golang support.
 ;;
