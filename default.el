b5c0929b400307a305a3c7df5042a70d6c7ab79c
Wesley Nelson <wgn@wesnel.dev>
2024-06-25 10:04:20 -0700

    Remove `consult` and related packages

--- default.el
+++ default.el
@@ -802,91 +802,6 @@
   :init
   (marginalia-mode +1))
 
-;; Search and navigation functions based on `completing-read'.
-(use-package consult
-  :ensure t
-
-  :bind
-  (;; C-c bindings in `mode-specific-map'
-   ("C-c M-x" . consult-mode-command)
-   ("C-c h" . consult-history)
-   ("C-c k" . consult-kmacro)
-   ("C-c m" . consult-man)
-   ("C-c i" . consult-info)
-   ([remap Info-search] . consult-info)
-   ;; C-x bindings in `ctl-x-map'
-   ("C-x M-:" . consult-complex-command)     ;; orig. repeat-complex-command
-   ("C-x b" . consult-buffer)                ;; orig. switch-to-buffer
-   ("C-x 4 b" . consult-buffer-other-window) ;; orig. switch-to-buffer-other-window
-   ("C-x 5 b" . consult-buffer-other-frame)  ;; orig. switch-to-buffer-other-frame
-   ("C-x t b" . consult-buffer-other-tab)    ;; orig. switch-to-buffer-other-tab
-   ("C-x r b" . consult-bookmark)            ;; orig. bookmark-jump
-   ("C-x p b" . consult-project-buffer)      ;; orig. project-switch-to-buffer
-   ;; Custom M-# bindings for fast register access
-   ("M-#" . consult-register-load)
-   ("M-'" . consult-register-store)          ;; orig. abbrev-prefix-mark (unrelated)
-   ("C-M-#" . consult-register)
-   ;; Other custom bindings
-   ("M-y" . consult-yank-pop)                ;; orig. yank-pop
-   ;; M-g bindings in `goto-map'
-   ("M-g e" . consult-compile-error)
-   ("M-g f" . consult-flymake)               ;; Alternative: consult-flycheck
-   ("M-g g" . consult-goto-line)             ;; orig. goto-line
-   ("M-g M-g" . consult-goto-line)           ;; orig. goto-line
-   ("M-g o" . consult-outline)               ;; Alternative: consult-org-heading
-   ("M-g m" . consult-mark)
-   ("M-g k" . consult-global-mark)
-   ("M-g i" . consult-imenu)
-   ("M-g I" . consult-imenu-multi)
-   ;; M-s bindings in `search-map'
-   ("M-s d" . consult-find)                  ;; Alternative: consult-fd
-   ("M-s c" . consult-locate)
-   ("M-s g" . consult-grep)
-   ("M-s G" . consult-git-grep)
-   ("M-s r" . consult-ripgrep)
-   ("M-s l" . consult-line)
-   ("M-s L" . consult-line-multi)
-   ("M-s k" . consult-keep-lines)
-   ("M-s u" . consult-focus-lines)
-   ;; Isearch integration
-   ("M-s e" . consult-isearch-history)
-   :map isearch-mode-map
-   ("M-e" . consult-isearch-history)         ;; orig. isearch-edit-string
-   ("M-s e" . consult-isearch-history)       ;; orig. isearch-edit-string
-   ("M-s l" . consult-line)                  ;; needed by consult-line to detect isearch
-   ("M-s L" . consult-line-multi)            ;; needed by consult-line to detect isearch
-   ;; Minibuffer history
-   :map minibuffer-local-map
-   ("M-s" . consult-history)                 ;; orig. next-matching-history-element
-   ("M-r" . consult-history))                ;; orig. previous-matching-history-element
-
-  :commands
-  (consult-register-format
-   consult-register-window
-   consult-xref)
-
-  :defines
-  (consult-narrow-key)
-
-  :hook
-  ;; Enable automatic preview at point in the *Completions* buffer:
-  (completion-list-mode . consult-preview-at-point-mode)
-
-  :init
-  ;; Optionally configure the register formatting.  This improves the
-  ;; register preview for `consult-register', `consult-register-load',
-  ;; `consult-register-store' and the Emacs built-ins.
-  (setq register-preview-delay 0.5
-        register-preview-function #'consult-register-format)
-
-  ;; Optionally tweak the register preview window.  This adds thin
-  ;; lines, sorting and hides the mode line of the window.
-  (advice-add #'register-preview :override #'consult-register-window)
-
-  ;; Use Consult to select xref locations with preview:
-  (setq xref-show-xrefs-function #'consult-xref
-        xref-show-definitions-function #'consult-xref))
-
 ;;;; Perform actions on point.
 (use-package embark
   :ensure t
@@ -961,32 +876,6 @@ targets."
                '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
                  nil
                  (window-parameters (mode-line-format . none)))))
-
-;; Integration between eglot and consult.
-(use-package consult-eglot
-  :ensure t
-
-  :commands
-  (consult-eglot-symbols))
-
-;; Integration between eglot and consult AND embark!
-(use-package consult-eglot-embark
-  :ensure t
-
-  :commands
-  (consult-eglot-embark-mode)
-
-  :init
-  (with-eval-after-load 'embark
-    (with-eval-after-load 'consult-eglot
-      (consult-eglot-embark-mode))))
-
-;; Integration between embark and consult.
-(use-package embark-consult
-  :ensure t
-
-  :hook
-  (embark-collect-mode . consult-preview-at-point-mode))
 
 ;;;; More helpful documentation for Emacs Lisp.
 (use-package helpful
