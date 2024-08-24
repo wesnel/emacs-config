46d136aa625b92f4a944565878bb1f6d288c3ba6
Wesley Nelson <wgn@wesnel.dev>
2024-08-23 19:54:19 -0700

    Remove some UI elements

--- default.el
+++ default.el
@@ -65,6 +65,8 @@
   :init
   ;; Remove some UI elements.
   (menu-bar-mode -1)
+  (tool-bar-mode -1)
+  (scroll-bar-mode -1)
 
   ;; Show line numbers at the beginning of each line.
   (global-display-line-numbers-mode +1)
