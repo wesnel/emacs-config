a7009735ec0eb13ac6eed8456366a08b150ff04a
Wesley Nelson <wgn@wgn.dev>
2025-10-20 23:44:29 -0700

    Attempt to fix issues with Copilot integration

--- default.el
+++ default.el
@@ -986,6 +986,7 @@
 
   :custom
   (copilot-server-executable "@copilot@")
+  (copilot-indent-offset-warning-disable t)
 
   :bind
   (:map copilot-completion-map
@@ -1013,9 +1014,9 @@
   (("C-RET" . #'gptel-send)
    ("C-<return>" . #'gptel-send))
 
-  :custom
-  (gptel-model 'claude-4.5-sonnet)
-  (gptel-backend (gptel-make-gh-copilot "Copilot")))
+  :config
+  (setq gptel-model 'claude-4.5-sonnet
+        gptel-backend (gptel-make-gh-copilot "Copilot")))
 
 ;;;; Improved `completing-read' functions.
 (use-package consult
